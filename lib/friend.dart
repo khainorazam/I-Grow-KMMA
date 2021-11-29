86ad7f64c79f1a659c7564e570d1cfb45a2dcde6
khainorazam <khainorazam25@gmail.com>
2021-11-30 00:19:54 +0800

    Friends update

--- lib/friend.dart
+++ lib/friend.dart
@@ -1,16 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_group9/login.dart';
 
 class Friends extends StatelessWidget {
   const Friends({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.lightGreen.shade100,
-        child: const Center(
-          child: Text('Friends'),
-        ),
+    return Container(
+      decoration: BoxDecoration(color: Colors.lightGreen.shade100),
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.all(45),
+                  child: const Text(
+                    'Friends',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    //friend 1
+                    margin: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 60.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Login()));
+                        },
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.lightGreen.shade300),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ))),
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              margin: const EdgeInsets.only(right: 10),
+                              child: const CircleAvatar(
+                                backgroundImage: AssetImage("assets/azam.jpg"),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                padding: const EdgeInsets.only(right: 8.0),
+                                margin:
+                                    const EdgeInsets.only(top: 16.0, left: 8.0),
+                                child: Column(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Container(
+                                        child: const Text(
+                                          'Khainor Azam',
+                                          style: TextStyle(
+                                              fontSize: 20.0,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Container(
+                                          child: const Text(
+                                        'Remove Friend',
+                                      )),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
