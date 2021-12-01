95b3bb47cb3cb97606105e9a5f2890a31704ff83
Firhish <86069053+Firhish@users.noreply.github.com>
2021-12-02 00:20:24 +0800

    Revert "Update 2 daus"
    This reverts commit 7499ca2e181362d31038cfc1fca7e565ed8a85c9.

--- lib/group.dart
+++ lib/group.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_group9/viewgroup.dart';
-import 'package:flutter_group9/widget/custom_page_route.dart';
 
 class Groups extends StatelessWidget {
   const Groups({Key? key}) : super(key: key);
@@ -56,9 +55,8 @@ class Groups extends StatelessWidget {
                         onPressed: (){
                            Navigator.push(
                              context,
-                             CustomPageRoute(
-                               child: const ViewGroup(),
-                               direction: AxisDirection.left,
+                             MaterialPageRoute(
+                               builder: (context) => const ViewGroup()
                                )
                               );
                         },
